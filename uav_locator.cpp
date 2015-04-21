b66afcb2231b169274948ed04ee90539755f1df2
Malthe Høj-Sunesen <alrekr@users.noreply.github.com>
2015-04-21 23:07:37 +0200

    Update uav_locator.cpp

--- uav_locator.cpp
+++ uav_locator.cpp
@@ -1,3 +1,7 @@
+/*
+ * TODO: Make into ROS node.
+ */
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -8,56 +12,40 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include "ros/ros.h"
+#include "ros/time.h"
 #include "uav_locator.hpp"
 
 using namespace cv;
 using namespace std;
 
 int i, best_match;
-cv::Mat src, in;
-std::vector<std::vector<cv::Point> > received;
+Mat src, in;
+vector<vector<Point> > received;
 double lowest = INT_MAX, match, theta;
-std::vector<double> matches;
-std::vector<std::vector<cv::Point> > original;
-p3d _p_here;
+vector<double> matches;
+vector<vector<Point> > original;
+Point3d _p_here;
 bool set_up_complete = false;
 
-p3d locate_uav(Mat _in) {
+Point3d locate_uav(Mat _in) {
     if(set_up_complete == false) {
         exit(-1);
     }
 
-    _p_here.cen_x = 0;
-    _p_here.cen_y = 0;
-    _p_here.theta = 4;
-#ifdef DEBUG
-    cout << "Loading input image" << endl;
-	_in = imread("/home/alrekr/Pictures/UAS/frame_194.png", CV_LOAD_IMAGE_GRAYSCALE);
-	cout << "Input image loaded" << endl;
-#endif //DEBUG
+    _p_here.x = 0;
+    _p_here.y = 0;
+    _p_here.z = 4;
+
     received = get_shapes(_in);
-#ifdef DEBUG
-	cout << "Shapes for input image received" << endl;
-	cout << "Contours in input image: " << received.size() << endl;
-#endif //DEBUG
+
     for(i = 0; i < (int)received.size(); i++) {
-#ifdef DEBUG
-		cout << "Run " << i << " in matching shapes" << endl;
-#endif //DEBUG
-/* FIXME: There's a snake in my boot! */
         matches.push_back(matchShapes(original[ORIGINAL_SHAPE], received[i], CV_CONTOURS_MATCH_I1, 0));
-#ifdef DEBUG
-		cout << "Match " << i << " completed" << endl;
-#endif //DEBUG
     }
-#ifdef DEBUG
-	cout << "Found matches, finding lowest number" << endl;
-#endif //DEBUG
+
     for(i = 0; i < (int)matches.size(); i++) {
         match = matches[i];
-#ifdef DEBUG
-        cout << "Match is " << match << endl;
-#endif //DEBUG
+
         if(lowest > match && match < MATCH_SHAPE_THRESHOLD) {
             lowest = match;
             best_match = i;
@@ -66,19 +54,7 @@ p3d locate_uav(Mat _in) {
 
     if (lowest != INT_MAX) {
         get_orientation(received, best_match, _p_here);
-        get_center(received, best_match, _p_here);
-#ifdef DEBUG
-        cout << "Angle is " << rtod(_p_here.theta) << endl;
-#endif //DEBUG
     }
-#ifdef DEBUG
-    else {
-        cout << "Best match was " << lowest << endl;
-    }
-#endif //DEBUG
-#ifdef DEBUG
-	cout << "Program is done." << endl;
-#endif //DEBUG
 	return _p_here;
 }
 
@@ -89,22 +65,13 @@ p3d locate_uav(Mat _in) {
  *****************************************************************************/
 void init_locate_uav(void) {
     Mat src = imread(SAMPLE_IMAGE, CV_LOAD_IMAGE_GRAYSCALE);
+
 	if(!src.data) {
-#ifdef DEBUG
-		cout << "Original image not loaded!!" << endl;
-#endif //DEBUG
+		cout << "Sample image not loaded. Did you remember to place 'sample.png' in the same folder as uav_locator.cpp?" << endl;
 		exit(-2);
 	}
-#ifdef DEBUG
-	else {
-		cout << "Original image loaded." << endl;
-	}
-#endif //DEBUG
+
     original = get_shapes(src);
-#ifdef DEBUG
-	cout << "Shapes in original image identified." << endl;
-	cout << "Contours in original image: " << original.size() << endl;
-#endif //DEBUG
     set_up_complete = true;
 }
 
@@ -150,33 +117,23 @@ vector<vector<Point> > get_shapes(Mat _src) {
 	erode_dilate(_src);
 	findContours(_src, _contours, _hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-
 	return _contours;
 }
 
 /*****************************************************************************
- * Calculates the orientation of the found shape, based on the found contours.
- * Function is based on
+ * Calculates the orientation and center of the found shape, based on the
+ * found contours. Function is based on
  * http://stackoverflow.com/questions/14720722/binary-image-orientation
- * Input: Found contours and which contour matches with the original sample.
- * Output: Angle in radians
+ * Input: Found contours
+ *        Which contour matches with the original sample
+ *        Address to an object of Point3d type
+ *
  *****************************************************************************/
-void get_orientation(vector<vector<Point> > _contours, int _which_one, p3d &_p) {
-	//based on
-	double _theta;
-	Moments _m = moments(_contours[_which_one], false);
-	_p.theta = 0.5 * atan2(((-2) * _m.mu11), (_m.mu20 - _m.mu02));
-}
-
-/*****************************************************************************
- * Calculates the center of the contour
- * Input: vector<Point>, address of resulting p3d
- * Output: None
- *****************************************************************************/
-void get_center(vector<vector<Point> > _contour, int _n, p3d &_p) {
-    Moments _m = moments(_contour[_n]);
-    _p.cen_x= _m.m10/_m.m00;
-    _p.cen_y = _m.m01/_m.m00;
+void get_orientation(vector<vector<Point> > _contours, int _n, Point3d &_p) {
+	Moments _m = moments(_contours[_n], false);
+    _p.x = _m.m10/_m.m00;
+    _p.y = _m.m01/_m.m00;
+	_p.z = 0.5 * atan2(((-2) * _m.mu11), (_m.mu20 - _m.mu02));
 }
 
 /*****************************************************************************
