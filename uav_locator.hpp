5c6362121d68af02c24247f8962284e7f3f763dd
Malthe Høj-Sunesen <alrekr@users.noreply.github.com>
2015-04-21 23:04:46 +0200

    Update uav_locator.hpp

--- uav_locator.hpp
+++ uav_locator.hpp
@@ -3,20 +3,13 @@
 #include <stdlib.h>
 #include <vector>
 
-typedef struct point3d {
-    double cen_x;
-    double cen_y;
-    double theta;
-    double max_length;
-} p3d;
-
-p3d locate_uav(cv::Mat);
+cv::Point3d locate_uav(cv::Mat);
 void init_locate_uav(void);
 void prepare_mat(cv::Mat&);
 void erode_dilate(cv::Mat&);
 std::vector<std::vector<cv::Point> > get_shapes(cv::Mat);
-void get_orientation(std::vector<std::vector<cv::Point> >, int, p3d&);
-void get_center(std::vector<std::vector<cv::Point> >, int, p3d&);
+void get_orientation(std::vector<std::vector<cv::Point> >, int, cv::Point3d&);
+//void get_center(std::vector<std::vector<cv::Point> >, int, cv::Point3d&);
 double rtod(double);
 double dtor(double);
 
